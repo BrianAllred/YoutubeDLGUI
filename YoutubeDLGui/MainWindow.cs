c6ca783223aad0e276f5bf3232dd5a521114345f
BrianAllred <brian.d.allred@gmail.com>
2015-04-25 08:00:10 -0500

    Huge GUI cleanup.

--- YoutubeDLGui/MainWindow.cs
+++ YoutubeDLGui/MainWindow.cs
@@ -26,127 +26,138 @@
 
 using System;
 using System.Collections.Generic;
-using Gdk;
 using Gtk;
 using YoutubeDL;
-using Window = Gtk.Window;
-using WindowType = Gtk.WindowType;
+using Key = Gdk.Key;
 
-namespace YoutubeDLGui{
-	public partial class MainWindow : Window
-	{
-
-	    public YoutubeDLController Controller = YoutubeDLController.Instance();
-
+namespace YoutubeDLGui
+{
+    public partial class MainWindow : Window
+    {
         private readonly List<char> _userPassword = new List<char>();
-        private List<char> _videoPassword = new List<char>();
+        private readonly List<char> _videoPassword = new List<char>();
+        public YoutubeDLController Controller = YoutubeDLController.Instance();
 
-	    public MainWindow() : base(WindowType.Toplevel)
-	    {
-	        Build();
-	    }
+        public MainWindow() : base(WindowType.Toplevel)
+        {
+            Build();
+        }
 
-	    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
-	    {
-	        Application.Quit();
-	        a.RetVal = true;
-	    }
+        protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+            a.RetVal = true;
+        }
 
-		protected void OnProxyCheckButtonToggled (object sender, System.EventArgs e)
-		{
-			proxyUrlTextView.Sensitive = proxyCheckButton.Active;
-		}
+        protected void OnProxyCheckButtonToggled(object sender, EventArgs e)
+        {
+            proxyUrlTextView.Sensitive = proxyCheckButton.Active;
+        }
 
-		protected void OnSocketTimeoutCheckButtonToggled (object sender, System.EventArgs e)
-		{
-			socketTimeoutTextView.Sensitive = socketTimeoutCheckButton.Active;
-		}
+        protected void OnSocketTimeoutCheckButtonToggled(object sender, EventArgs e)
+        {
+            socketTimeoutTextView.Sensitive = socketTimeoutCheckButton.Active;
+        }
 
-		protected void OnRateLimitCheckButtonToggled (object sender, System.EventArgs e)
-		{
-			rateTextView.Sensitive = rateLimitCheckButton.Active;
-			rateUnitComboBox.Sensitive = rateLimitCheckButton.Active;
-		}
+        protected void OnRateLimitCheckButtonToggled(object sender, EventArgs e)
+        {
+            rateTextView.Sensitive = rateLimitCheckButton.Active;
+            rateUnitComboBox.Sensitive = rateLimitCheckButton.Active;
+        }
 
-		protected void OnRetriesCheckButtonToggled (object sender, System.EventArgs e)
-		{
-			retriesTextView.Sensitive = retriesCheckButton.Active;
-		}
+        protected void OnRetriesCheckButtonToggled(object sender, EventArgs e)
+        {
+            retriesTextView.Sensitive = retriesCheckButton.Active;
+        }
 
-		protected void OnUsernameTextViewKeyReleaseEvent (object o, KeyReleaseEventArgs args)
-		{
-		    if (usernameTextView.Buffer.Text.Length == 0)
-		    {
+        protected void OnUsernameTextViewKeyReleaseEvent(object o, KeyReleaseEventArgs args)
+        {
+            if (usernameTextView.Buffer.Text.Length == 0)
+            {
                 Controller.Username = string.Empty;
             }
-		    else
-		    {
-		        Controller.Username = usernameTextView.Buffer.Text;
-		    }
-		}
-
-		protected void OnPasswordTextViewKeyReleaseEvent (object o, KeyReleaseEventArgs args)
-		{
-
-            if (passwordTextView.Buffer.Text.Length == 0)
+            else
             {
-                _userPassword.Clear();
+                Controller.Username = usernameTextView.Buffer.Text;
+            }
+        }
+
+        protected void OnPasswordTextViewKeyReleaseEvent(object o, KeyReleaseEventArgs args)
+        {
+            ProcessPasswordText(passwordTextView, _userPassword, args);
+            Controller.Password = new string(_userPassword.ToArray());
+        }
+
+        protected void OnVideoPasswordTextViewKeyReleaseEvent(object o, KeyReleaseEventArgs args)
+        {
+            ProcessPasswordText(videoPasswordTextView, _videoPassword, args);
+            Controller.VideoPassword = new string(_videoPassword.ToArray());
+        }
+
+        protected void OnUsernameTextViewFocusOutEvent(object o, FocusOutEventArgs args)
+        {
+            if (string.IsNullOrEmpty(usernameTextView.Buffer.Text))
+            {
+                usernameTextView.Buffer.Text = "Username";
+            }
+        }
+
+        protected void OnPasswordTextViewFocusOutEvent(object o, FocusOutEventArgs args)
+        {
+            if (string.IsNullOrEmpty(passwordTextView.Buffer.Text))
+            {
+                passwordTextView.Buffer.Text = "Password";
+            }
+        }
+
+        protected void OnPasswordCheckButtonToggled(object sender, EventArgs e)
+        {
+            if (_userPassword.Count > 0)
+            {
+                passwordTextView.Buffer.Text = passwordCheckButton.Active
+                    ? new string(_userPassword.ToArray())
+                    : new string('*', _userPassword.Count);
+            }
+        }
+
+        protected void ProcessPasswordText(TextView textView, List<char> localPassword, KeyReleaseEventArgs args)
+        {
+            if (textView.Buffer.Text.Length == 0)
+            {
+                localPassword.Clear();
             }
             else
             {
                 if (args.Event.KeyValue > 32 && args.Event.KeyValue < 123)
                 {
-                    passwordTextView.Buffer.Text = string.Empty;
-                    if (args.Event.Key == Gdk.Key.BackSpace)
+                    textView.Buffer.Text = string.Empty;
+                    if (args.Event.Key == Key.BackSpace)
                     {
-                        if (_userPassword.Count > 0)
+                        if (localPassword.Count > 0)
                         {
-                            _userPassword.RemoveAt(_userPassword.Count - 1);
-                            Controller.Password = new string(_userPassword.ToArray());
+                            localPassword.RemoveAt(localPassword.Count - 1);
                         }
                     }
                     else
                     {
-                        _userPassword.Add((char) args.Event.KeyValue);
-                        Controller.Password = new string(_userPassword.ToArray());
+                        localPassword.Add((char) args.Event.KeyValue);
                     }
 
-                    passwordTextView.Buffer.Text = passwordCheckButton.Active
-                        ? new string(_userPassword.ToArray())
-                        : new string('*', _userPassword.Count);
+                    textView.Buffer.Text = passwordCheckButton.Active
+                        ? new string(localPassword.ToArray())
+                        : new string('*', localPassword.Count);
                 }
             }
         }
 
-		protected void OnVideoPasswordTextViewKeyReleaseEvent (object o, KeyReleaseEventArgs args)
-		{
-			throw new NotImplementedException ();
-		}
-
-		protected void OnUsernameTextViewFocusOutEvent (object o, FocusOutEventArgs args)
-		{
-            if (string.IsNullOrEmpty(usernameTextView.Buffer.Text))
+        protected void OnVideoPasswordCheckButtonToggled(object sender, EventArgs e)
+        {
+            if (_videoPassword.Count > 0)
             {
-                usernameTextView.Buffer.Text = "Username";
+                videoPasswordTextView.Buffer.Text = videoPasswordCheckButton.Active
+                    ? new string(_videoPassword.ToArray())
+                    : new string('*', _videoPassword.Count);
             }
-		}
-
-		protected void OnPasswordTextViewFocusOutEvent (object o, FocusOutEventArgs args)
-		{
-			if (string.IsNullOrEmpty(passwordTextView.Buffer.Text))
-			{
-				passwordTextView.Buffer.Text = "Password";
-			}
-		}
-
-		protected void OnPasswordCheckButtonToggled (object sender, EventArgs e)
-		{
-		    if (_userPassword.Count > 0)
-		    {
-		        passwordTextView.Buffer.Text = passwordCheckButton.Active
-		            ? new string(_userPassword.ToArray())
-		            : new string('*', _userPassword.Count);
-		    }
-		}
-	}
+        }
+    }
 }
